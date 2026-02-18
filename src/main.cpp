13eabfa632c6c04bf93b3287e4afd10080b8655b
Alam Barkani <alambarkani@gmail.com>
2026-02-18 22:53:21 +0700

    feat(player): add player sprite and early controller (WIP)

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,66 @@
 #include <SFML/Graphics.hpp>
+#include <cmath>
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
-	sf::RectangleShape rectangle({100.f, 100.f});
-	rectangle.setFillColor(sf::Color::Red);
-	rectangle.setOrigin({50.f, 50.f});
+	sf::RenderWindow window(sf::VideoMode({1280, 600}), "Snake Game");
+	window.setVerticalSyncEnabled(true);
+	window.setFramerateLimit(60);
 
-	float rotationSpeed = 100.f;
+	// Player Sprite
+	sf::RectangleShape player({20.f, 20.f});
+	player.setFillColor(sf::Color::Green);
+	player.setOutlineColor(sf::Color::Black);
+	player.setOutlineThickness(1.f);
+	player.setPosition({100.f, 100.f});
+
+	player.setOrigin({25.f, 25.f});
+
+	const float playerSpeed = 100.f;
+
 	sf::Clock clock;
+
 	while (window.isOpen())
 	{
-		sf::Time deltaTime = clock.restart();
-		float dt = deltaTime.asSeconds();
 		while (const std::optional event = window.pollEvent())
 		{
 			if (event->is<sf::Event::Closed>())
+			{
 				window.close();
+			}
 		}
 
-		rectangle.rotate(sf::degrees(rotationSpeed * dt));
+		sf::Time elapsed = clock.restart();
+		float deltaTime = elapsed.asSeconds();
 
-		window.clear();
-		window.draw(shape);
-		window.draw(rectangle);
+		sf::Vector2f velocity{0.f, 0.f};
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+		{
+			velocity.y -= 1.f;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+		{
+			velocity.y += 1.f;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+		{
+			velocity.x -= 1.f;
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+		{
+			velocity.x += 1.f;
+		}
+
+		if (velocity.x != 0.f && velocity.y != 0.f)
+			velocity /= std::sqrt(2.f);
+
+		player.move(velocity * deltaTime * playerSpeed);
+
+		window.clear(sf::Color::White);
+		window.draw(player);
 		window.display();
 	}
+
+	return 0;
 }
